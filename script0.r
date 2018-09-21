8a0c91e603d79cf9e9e29835579de04eeaef3be0
Jordi Armengol Estapé <jordi.armengol.estape@a5s113pc36.fib.upc.es>
2018-09-21 16:06:23 +0200

    pre

--- script0.r
+++ script0.r
@@ -5,3 +5,7 @@ na_count <- data.frame(na_count)
 nrow(mydata)
 sum(!is.na(mydata))
 sum(is.na(mydata))/sum(!is.na(mydata))*100
+
+mydata2 <- mydata [!(mydata$wave==5 | mydata$wave ==6 | mydata$wave ==7 | mydata$wave ==8 | mydata$wave ==9 | mydata$wave ==12 | mydata$wave ==13 | mydata$wave ==14 | mydata$wave ==18 | mydata$wave ==19 | mydata$wave ==20 | mydata$wave ==21),]
+
+mydata3 <- subset( mydata2, select = -c( attr1_s : amb5_3 ))
